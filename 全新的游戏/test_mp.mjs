825ce962c5dcccfbcef5b7677015831ebc93dde7
ccb982 <2264184962@qq.com>
2026-08-17 22:56:52 +0800

    暑假就安心搓项目

--- 全新的游戏/test_mp.mjs
+++
@@ -1,3 +0,0 @@
-import { buildSync } from 'esbuild';
-buildSync({ entryPoints: ['test_mp_entry.ts'], bundle: true, outfile: 'test_mp.cjs', format: 'cjs', platform: 'node', logLevel: 'error' });
-console.log('bundled');
--- 另起的绘画网页/fix_bundle.mjs
+++
@@ -1,8 +0,0 @@
-import { build } from 'esbuild';
-import { writeFileSync } from 'fs';
-const result = await build({
-  entryPoints: ['C:/Users/22641/Desktop/架构重置/另起的绘画网页/src/utils/colorCompressor.ts'],
-  bundle: true, format: 'cjs', write: false, platform: 'node', logLevel: 'silent',
-});
-writeFileSync('C:/Users/22641/Desktop/架构重置/另起的绘画网页/_fix_bundle.cjs', result.outputFiles[0].contents);
-console.log('ok');
